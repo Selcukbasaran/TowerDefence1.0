941802ff8e70d98590815321b83fd36c5c105b2b
Selçuk Başaran <selcukbasaran41@gmail.com>
2021-05-09 00:19:39 +0300

    OnMouseDown tower placement

---
+++ Assets/TowerBase.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBase : MonoBehaviour
+{
+    //renk verme
+    [Header("Secim rengi")]
+    public Color new_color;
+
+    private SpriteRenderer spriteRenderer;
+    //private Renderer rend;
+    private Color startcolor;
+    //renk verme bitiþ
+
+    private GameObject tower;
+
+    public Vector3 positionOffset;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        //spriteRenderer.material.color = Color.red;
+        startcolor = spriteRenderer.material.color;
+    }
+
+
+
+    private void OnMouseDown()
+    {
+        if (tower != null)
+        {
+            Debug.Log("Zaten kule var! - TODO: Bu kýsým kullanýcý için ekrana yazýlsýn");
+            return;
+        }
+
+        // Kule kurma
+        GameObject towerToBuild = TowerBuilder.instance.GetTowerToBuild();
+        tower = (GameObject)Instantiate(towerToBuild, transform.position + positionOffset, transform.rotation);
+
+    }
+
+
+    void OnMouseEnter()
+    {
+        spriteRenderer.material.color = new_color;
+        Debug.Log("Mouse is over GameObject.");
+    }
+
+    void OnMouseExit()
+    {
+        spriteRenderer.material.color = startcolor;
+    }
+
+
+}
